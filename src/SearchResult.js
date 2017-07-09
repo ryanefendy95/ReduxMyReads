acb63afb6caf4f02cbd5a46457ebdb5bbfe0ce7d
Ryan Efendy <ryanefendy95@gmail.com>
2017-07-08 18:25:44 -0700

    rm SearchGrid refactor search page

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,15 +1,27 @@
 import React from 'react'
-import {SearchGrid} from './SearchGrid'
+import Book from './Book'
 import './App.css'
 
 export const SearchResult = (props) => {
+    // moveBook = this.props.onMoveBook;
+    const bookList = props.books.map((book, i) => {
+        return (
+            <Book
+                key={i}
+                title={book.title}
+                authors={book.authors}
+                style={{width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}
+                book={book}
+                value={props.value}
+            />
+        )
+    });
+
     return (
         <div className="search-books-results">
             <ol className="books-grid">
-                <SearchGrid
-                    books={props.books}
-                />
+                {bookList}
             </ol>
         </div>
     )
-}
+};
