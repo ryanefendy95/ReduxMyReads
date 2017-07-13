c354c1150af5778cb80d19e24fba1acfa57599ed
Ryan Efendy <ryanefendy95@gmail.com>
2017-07-13 09:57:00 -0700

    refactor code clean up

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,42 +1,22 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {Link} from 'react-router-dom'
 import './App.css'
 
-export default class SearchBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {term: ''};
-    }
-
-    // onInputChange(term) {
-    //     if(term) {
-    //         this.setState({term: term.trim()});
-    //         this.props.onSearch(term.trim());
-    //     } else {
-    //         this.setState({term: ''});
-    //     }
-    // };
-
-    onInputChange(term) {
-        this.props.onInputChange(term);
-    }
-
-    render() {
-        return (
-            <div className="search-books-bar">
-                <Link className="close-search" to="/">Close</Link>
-                <div className="search-books-input-wrapper">
-                    <input
-                        value={this.props.term}
-                        type="text"
-                        placeholder="Search by title or author"
-                        onChange={event => this.onInputChange(event.target.value)}
-                    />
-                </div>
+export const SearchBar = (props) => {
+    return (
+        <div className="search-books-bar">
+            <Link className="close-search" to="/">Close</Link>
+            <div className="search-books-input-wrapper">
+                <input
+                    value={props.term}
+                    type="text"
+                    placeholder="Search by title or author"
+                    onChange={event =>  props.onInputChange(event.target.value)}
+                />
             </div>
-        )
-    }
-}
+        </div>
+    )
+};
 
 // static propTypes = {
 //     booksOnShelf: PropTypes.array,
