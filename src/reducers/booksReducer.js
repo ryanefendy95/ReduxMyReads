8c0efd22b3a9c09051abc0328bd0c86e5eddf3d7
ryan efendy <ryanefendy95@gmail.com>
2017-07-14 15:00:47 -0700

    testing

--- src/reducers/booksReducer.js
+++ src/reducers/booksReducer.js
@@ -1,6 +1,8 @@
 import {FETCH_BOOKS} from '../actions/index';
 
 export default function (state = [], action) {
+    console.log('inside bookReducer BEFORE', state);
+
     // never mutate new state!!!
     switch (action.type) {
         case FETCH_BOOKS:
@@ -9,6 +11,7 @@ export default function (state = [], action) {
     }
 
     // console.log('Action received', action);
+    console.log('inside bookReducer AFTER', state);
 
     return state;
 }
--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -1,11 +1,16 @@
 import {FETCH_SEARCH} from '../actions/index';
 
 export default function (state = [], action) {
+    console.log('inside searchReducer BEFORE', state);
+
     switch (action.type) {
         case FETCH_SEARCH:
+
             return action.payload;
         default:
     }
+
+    console.log('inside searchReducer AFTER', state);
 
     return state;
 }
